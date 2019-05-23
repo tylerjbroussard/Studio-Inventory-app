6a48fb98f0682e66928a6b8e445d702d65092271
tyler <tylerjbroussard@live.com>
2019-05-23 15:49:50 -0700

    yes

---
+++ Assignment07/Models/InventoryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment07.Models
+{
+    public class ItemData
+    {
+        public object ItemCode;
+        public string Description;
+        public double PricePerItem;
+        public int QuantityOnHand;
+        public double OurCostPerItem;
+        public double ValueOfItem;
+
+    }
+}
